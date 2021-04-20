7787605a32793bea8c46a37a75d65be8c6e74fc4
abhilash <abhilash.kamble376@gmail.com>
2021-04-20 12:04:23 +0530

    canvas

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # my practices 
  
+## Cookies
+    - Session
+    - Local
+    - IndexBD
+    
--- js/script.js
+++ js/script.js
@@ -1,3 +1,25 @@
+function addGraphics() {
+    //logic to add graphics inside canvas
+    let cv = document.querySelector("#cvs");
+    let context = cv.getContext("2d");
+   
+    //circle
+    context.beginPath()
+    context.arc(200, 200, 50, 0,60)
+    context.stroke()
+    // line
+    context.moveTo(10, 10);
+    context.lineTo(150, 150);
+    context.stroke()
+   
+    // triangle
+    context.moveTo(10, 10);
+    context.lineTo(150, 150);
+    context.lineTo(300, 10)
+    context.fill()
+    context.stroke();
+  }
+    
 function setCookie() {
     document.cookie = "address=pune";
     document.cookie = "token=temptoken";
@@ -27,6 +49,7 @@ function setSessionStorage() {
 function getSessionStorage() {
     console.log(`auth token `+sessionStorage.getItem('auth'))
 }
+
 
 
 
